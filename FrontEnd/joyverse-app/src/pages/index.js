78aa4a6072687c54d03f4ae8c6a4d26bdb213c3f
RoshanMatthew2005 <roshanmatthew11@gmail.com>
2025-06-23 21:08:30 +0530

    yo

---
+++ FrontEnd/joyverse-app/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as WelcomePage } from './WelcomePage';
+export { default as SignUpPage } from './SignUpPage';
+export { default as TherapistSignUp } from './TherapistSignUp';
+export { default as ChildSignUp } from './ChildSignUp';
+export { default as LoginPage } from './LoginPage';
+export { default as ChildDashboard } from './ChildDashboard';
