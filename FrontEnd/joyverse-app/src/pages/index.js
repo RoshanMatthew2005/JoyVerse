27f0c7dd74d51a776b214fd135db7e3e5bb697fe
Bhuvan Reddy <bhuvanreddy114@gmail.com>
2025-07-14 12:10:29 +0530

    del

--- FrontEnd/joyverse-app/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as WelcomePage } from './WelcomePage';
-export { default as SignUpPage } from './SignUpPage';
-export { default as TherapistSignUp } from './TherapistSignUp';
-export { default as ChildSignUp } from './ChildSignUp';
-export { default as LoginPage } from './LoginPage';
-export { default as ChildDashboard } from './ChildDashboard';
-export { default as TherapistDashboard } from './TherapistDashboard';
